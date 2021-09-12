367e0b051baaa41ac577deebe31895aae7ef8202
Pavel <potehken.pasha91@gmail.com>
2021-09-12 13:08:33 +0700

    Add files

--- main.py
+++
@@ -1,6 +0,0 @@
-from blockdiag import parser, builder, drawer
-tree = parser.parse_string(sour)
-diagram = builder.ScreenNodeBuilder.build(tree)
-draw = drawer.DiagramDraw('PNG', diagram, filename="foo.png")
-draw.draw()
-draw.save()
